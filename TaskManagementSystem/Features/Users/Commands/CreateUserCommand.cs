a9b817da05a957889bc7294f3a95ad990f13583e
Sanjoy Kumar Mahato <sanjoymahato.2356.csedu@gmail.com>
2025-09-02 11:29:55 +0600

    add user crud with CQRS

---
+++ TaskManagementSystem/Features/Users/Commands/CreateUserCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace TaskManagementSystem.Features.Users.Commands
+{
+    public class CreateUserCommand: IRequest<CreateUserResponse>
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string RoleId { get; set; } = string.Empty;
+    }
+    public class CreateUserResponse
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
---
+++ TaskManagementSystem/Features/Users/Handlers/DeleteUserCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using TaskManagementSystem.Features.Users.Commands;
+using TaskManagementSystem.Repository.IRepository;
+
+namespace TaskManagementSystem.Features.Users.Handlers
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, UserDeleteResponse>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public DeleteUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserDeleteResponse> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var UserExist = await _userRepository.GetById(request.Id);
+            if (UserExist == null)
+            {
+                return new UserDeleteResponse
+                {
+                    Status = false,
+                    Message = "Failed to delete user as User does not exist in the system."
+                };
+            }
+            _userRepository.Delete(UserExist);
+            return new UserDeleteResponse { 
+                Status = true,
+                Message = "User deleted successfully."
+            };
+        }
+    }
+}
