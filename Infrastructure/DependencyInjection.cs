91104b7225ec0139e57d45b686eb9683b4b18ddd
Sanjoy Kumar Mahato <sanjoymahato.2356.csedu@gmail.com>
2026-04-26 15:56:12 +0600

    Set up clean Architecture

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastruture
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicaton(this IServiceCollection services)
+        {
+            var assembly = typeof(DependencyInjection).Assembly;
+            services.AddMediatR(configuration =>
+                configuration.RegisterServicesFromAssembly(assembly));
+            services.AddValidatorsFromAssembly(assembly);
+            return services;
+        }
+    }
+}
