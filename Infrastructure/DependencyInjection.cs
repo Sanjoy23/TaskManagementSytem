08e7f737e0809521b7208c5533cebc4cf4a52390
Sanjoy Kumar Mahato <sanjoymahato.2356.csedu@gmail.com>
2026-04-26 16:01:39 +0600

    Add packages

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -6,12 +6,9 @@ namespace Infrastruture
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplicaton(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
-            var assembly = typeof(DependencyInjection).Assembly;
-            services.AddMediatR(configuration =>
-                configuration.RegisterServicesFromAssembly(assembly));
-            services.AddValidatorsFromAssembly(assembly);
+            
             return services;
         }
     }
